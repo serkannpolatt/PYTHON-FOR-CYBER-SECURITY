5853bcfaf333982c4f1f194e6f1e4c7256108b3d
Serkan Polat <itsonlydatahustle@gmail.com>
2023-04-22 09:39:39 +0300

    A VPN control tool is a software application that allows users to manage and control their VPN connections.

---
+++ VPN Control Tool/vpn_control.py
@@ -0,0 +1,13 @@
+import os
+
+# Install figlet using the apt-get package manager
+os.system("apt-get install figlet")
+
+# Clear the terminal screen
+os.system("clear")
+
+# Use figlet to create a large ASCII banner for the title
+os.system("figlet VPN Kontrol Araci")
+
+# Print a welcome message
+print("Welcome to the VPN Control Tool!")
